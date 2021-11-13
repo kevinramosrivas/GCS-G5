a82753ba1b65f0982eeffc32047594d999b86c8d
Vivian Lazaro <vivian.lazaro@unmsm.edu.pe>
2021-11-12 22:23:22 -0500

    se completo la condicional para la barra lateral

--- Desarrollo/SACNS/codigo/Modulo_Docente_Frontend/0. SideBar/sidebar.js
+++ Desarrollo/SACNS/codigo/Modulo_Docente_Frontend/0. SideBar/sidebar.js
@@ -3,3 +3,12 @@ if(sidebar.classList.contains("open")){
     document.getElementById('hogar2').style.display='block';
     document.getElementById('nav-list').classList.remove('d-none');
      closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+
+    }else {
+        closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+        document.getElementById('hogar').style.display='none';
+        document.getElementById('hogar2').style.display='none';
+       document.getElementById('nav-list').classList.add('d-none');
+      }
+     }
+     

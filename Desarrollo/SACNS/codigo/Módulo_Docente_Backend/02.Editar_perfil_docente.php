84d744c8163847005a5e2404a6a6b59b92eb1eb7
Adriana Isabel <adriana.balceda@unmsm.edu.pe>
2021-11-18 14:44:04 -0500

    corrigiendo errores del perfil_docente

--- Desarrollo/SACNS/codigo/Módulo_Docente_Backend/02.Editar_perfil_docente.php
+++ Desarrollo/SACNS/codigo/Módulo_Docente_Backend/02.Editar_perfil_docente.php
@@ -9,7 +9,7 @@
   <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-  <link rel="stylesheet" href="assets/css/sidebar.css">
+  <link rel="stylesheet" href="assets/0.Sidebar/sidebar.css">
 </head>
 <body>
     <?php require_once('includes/sidebar_docente.php') ?>

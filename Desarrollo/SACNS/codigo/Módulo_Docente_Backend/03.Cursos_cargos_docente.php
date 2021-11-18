322527efecab0344ace13f8e869dfb7ceac32563
Adriana Isabel <adriana.balceda@unmsm.edu.pe>
2021-11-18 14:58:27 -0500

    enlazando estilos en Cursos_cargos_docente

--- Desarrollo/SACNS/codigo/Módulo_Docente_Backend/03.Cursos_cargos_docente.php
+++ Desarrollo/SACNS/codigo/Módulo_Docente_Backend/03.Cursos_cargos_docente.php
@@ -9,8 +9,8 @@
   <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="assets/css/cursos_cargo.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/sidebar.css">
+    <link href="assets/2.Notas/P1.Cursos_cargo_Docente.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/0.Sidebar/sidebar.css">
     <style type="text/css">
       .btn {
           display       : inline-block;

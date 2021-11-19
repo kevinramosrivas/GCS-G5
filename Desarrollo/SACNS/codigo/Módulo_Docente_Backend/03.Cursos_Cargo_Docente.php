2d980134ac88f084b6f7ec882ad966feabbb6a9f
Adriana Isabel <adriana.balceda@unmsm.edu.pe>
2021-11-19 13:53:28 -0500

    reordenando archivo de estilos cursos a cargo

--- Desarrollo/SACNS/codigo/Módulo_Docente_Backend/03.Cursos_Cargo_Docente.php
+++ Desarrollo/SACNS/codigo/Módulo_Docente_Backend/03.Cursos_Cargo_Docente.php
@@ -9,8 +9,8 @@
   <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="assets/2.Notas/P1.Cursos_cargo_Docente.css" rel="stylesheet">
-    <link rel="stylesheet" href="assets/0.Sidebar/sidebar.css">
+    <link href="assets/css/P1.Cursos_cargo_Docente.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/sidebar.css">
     <style type="text/css">
       .btn {
           display       : inline-block;
@@ -143,7 +143,7 @@
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="assets/0.Sidebar/sidebar.js"></script>
+    <script src="assets/css/sidebar.js"></script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--

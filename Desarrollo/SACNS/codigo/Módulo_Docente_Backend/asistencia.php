c1f389735e01a092bc3f6c19112f93eb100ef97a
KEVIN RAMOS <mc6.62x10.34@gmail.com>
2021-11-21 22:08:45 -0500

    se añaden las pantallas de asistencia y se mejora la pantalla de docentes cursos a cargo

--- Desarrollo/SACNS/codigo/Módulo_Docente_Backend/asistencia.php
+++ Desarrollo/SACNS/codigo/Módulo_Docente_Backend/asistencia.php
@@ -5,46 +5,25 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title> Cursos a cargo - Docente</title>
+	<title>Asistencia</title>
   <link rel="shortcut icon" href="assets/img/logo.png" type="image/x-icon">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link href="assets/css/cursos_cargo.css" rel="stylesheet">
+  <link href="assets/css/asistencia.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/sidebar.css">
-    <style type="text/css">
-      .btn {
-          display       : inline-block;
-          font-size     : 1rem;
-          padding       : .0.5rem;
-      }
-      .btn:hover {
-          cursor: pointer;
-      }
-      .custom-btn {
-          font-family : monospace;
-          margin      : 0.5rem;
-          padding-top : .0.5rem;
-      }
-      .vertical {
-          transform: rotate(-90deg);
-      }
-  </style>
+
 </head>
   <body>
     <?php require_once('includes/sidebar_docente.php') ?>
    
-    <div class="container mt-5 ml-5 p-5">
+    <div class="container p-5 mt-4 ">
       <div class="container-fluid p-0">
-      <h1 class="h3">Cursos a Cargo</h1>
+      <h1 class="h3">Asistencia de Salones a Cargo</h1>
       <div class="row">
         <div class="col-xl-8">
           <div class="card">
             <div class="card-header pb-0">
-              <div class="card-actions float-right">
-                
-              </div>
-
-              <h6 class="sub2"><img src="assets/img/chack.png" width="30" height="30" class="rounded-circle my-n1" alt="Avatar"> Numero de Cursos: 5</h6>
+              <h6 class="sub2"><img src="assets/img/chack.png" width="30" height="30" class="rounded-circle my-n1" alt="Avatar"> Numero de Cursos</h6>
             </div>
             <div class="card-body">
               <table class="table table-striped" style="width:100%" style="height:100;">
@@ -52,31 +31,29 @@
                   <tr>
                     <th>#</th>
                     <th>Curso</th>
-                    <th>Grado</th>
+                    <th>Docente</th>
                     <th>Estado</th>
-                    <th> </th>
+                    <th>Ingresar</th>
                   </tr>
                 </thead>
                 <tbody>
-
-                  <?php 
+                  <?php
                     for($i=0; $i<5; $i++){
 
                       ?>
-                  <tr id="lol">
-                      <td><img src="assets/img/<?php echo $_SESSION['datos_usuario']['especialidad'] ?>.png" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
-                      <td> <?php echo $_SESSION['datos_usuario']['especialidad'] ?> </td>
-                      <td><?php echo ($i+1) ?> Año</td>
-                      <td><div class="progress" style="height: 3px;">
-                        <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                      </div>25%</td>
-                      <td> <div class="btn custom-btn vertical">
-                        ...
-                      </div></td>
-                  </tr>
-                    
-                      <?php
+                      <tr>
+                        <td><img src="assets/img/<?php echo $_SESSION['datos_usuario']['especialidad'] ?>.png" width="32" height="32" class="rounded-circle my-n1" alt="Avatar"></td>
+                        <td><?php echo $_SESSION['datos_usuario']['especialidad'] ?></td>
+                        <td><?php echo ($i+1) ?> Año</td>
+                        <td><div class="progress" style="height: 3px;">
+                          <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>25%</td>
+                        <td><a href="registrar_asistencia.php?" class="btn btn-primary btn-sm">Registrar</a></td>
+                      </tr>
+
+                  <?php
                     }
+                  
                   ?>
                 </tbody>
               </table>

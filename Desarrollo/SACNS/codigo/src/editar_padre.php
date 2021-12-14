5955acc49f3cccb2c755b8be92a482bf91cf8536
KEVIN RAMOS <mc6.62x10.34@gmail.com>
2021-12-14 18:11:17 -0500

    se añade la logica a la pantalla de editar perfil padre

--- Desarrollo/SACNS/codigo/src/editar_padre.php
+++ Desarrollo/SACNS/codigo/src/editar_padre.php
@@ -4,17 +4,14 @@
     session_start();
     $celular = $_POST['celular'];
     $email = $_POST['email'];
-    $direccion = $_POST['direccion'];
     $contrasenia = $_POST['contrasenia'];
 
     if($contrasenia == ''){
         $contrasenia = $_SESSION['datos_usuario']['contrasenia'];
     }
 
-    $sql = "UPDATE docente SET celular = '$celular', email = '$email', contrasenia = '$contrasenia' WHERE docente_id = ".$_SESSION['datos_usuario']['docente_id'];
-
+    $sql = "UPDATE `padre` SET `celular` = '$celular', email = '$email', contrasenia = '$contrasenia' WHERE padre_id = ".$_SESSION['datos_usuario']['padre_id']; 
     mysqli_query($conexion, $sql);
-
     $_SESSION['datos_usuario']['celular'] = $celular;
     $_SESSION['datos_usuario']['email'] = $email;
     $_SESSION['datos_usuario']['contrasenia'] = $contrasenia;
